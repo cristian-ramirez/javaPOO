97d81061618008f1d6b7ded2205c65bea2d4259e
Cristian Ramirez <ramireze4@gmail.com>
2016-05-18 22:57:10 -0600

    Add images

--- polymorphism/Main.java
+++ polymorphism/Main.java
@@ -9,14 +9,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Number integer1 = new Integer(1);
-        Number integer2 = new Integer(2);
-        Number integer3 = new Integer(3);
-        
-        numbers.add(integer1);
-        numbers.add(integer2);
-        numbers.add(integer3);
-        
         Number float1 = new Float((float) 1.1);
         Number float2 = new Float((float) 2.2);
         Number float3 = new Float((float) 3.3);
@@ -24,6 +16,14 @@ public class Main {
         numbers.add(float1);
         numbers.add(float2);
         numbers.add(float3);
+        
+        Number integer1 = new Integer(1);
+        Number integer2 = new Integer(2);
+        Number integer3 = new Integer(3);
+        
+        numbers.add(integer1);
+        numbers.add(integer2);
+        numbers.add(integer3);
 
         numbers.stream().forEach(Number::printed);
     }
